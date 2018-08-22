19d446591243d3f264b7ce10f1b28116502dbfd8
HelloRuiLi <rrli@thoughtworks.com>
2018-08-22 23:10:19 +0800

    fix label display issue in CustomInput and add default value for it

--- src-components/CustomInput.tsx
+++ src-components/CustomInput.tsx
@@ -4,9 +4,9 @@ import Input from "@material-ui/core/Input/Input";
 import InputLabel from "@material-ui/core/InputLabel/InputLabel";
 import * as React from "react";
 
-export const CustomInput = ({ name, value, error, onChange, placeholder, type }: any) => (
+export const CustomInput = ({ name, value = "", label, error, onChange, placeholder, type }: any) => (
   <FormControl error={!!error}>
-    <InputLabel htmlFor={name}>{name}</InputLabel>
+    {label && <InputLabel>{label}</InputLabel>}
     <Input
       name={name}
       value={value}
