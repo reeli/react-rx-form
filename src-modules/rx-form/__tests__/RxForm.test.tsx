1e02091f4063232d322cba1e66a7364ab36f8c1f
reeli <reeli.null@gmail.com>
2018-08-26 15:36:54 +0800

    [Rui] add a simple test for RxForm

---
+++ src-modules/rx-form/__tests__/RxForm.test.tsx
@@ -0,0 +1,37 @@
+import { mount } from "enzyme";
+import * as React from "react";
+import { CustomInput } from "../../../src-components/CustomInput";
+import { Field } from "../Field";
+import { RxForm } from "../RxForm";
+
+describe("<RxForm/>", () => {
+  it("should correctly set initial values", () => {
+    const submit = () => {};
+    const wrapper = mount(
+      <RxForm>
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit(submit)}>
+            <div>
+              <Field name="firstName">
+                {(fieldProps) => <CustomInput {...fieldProps} type="text" placeholder="First Name" />}
+              </Field>
+            </div>
+          </form>
+        )}
+      </RxForm>,
+    );
+
+    const instance = wrapper.instance() as any;
+    instance.setInitialValues({ firstName: "rui" });
+    expect(instance.formState).toEqual({
+      firstName: {
+        name: "firstName",
+        value: "rui",
+        meta: {
+          error: undefined,
+          dirty: false,
+        },
+      },
+    });
+  });
+});
