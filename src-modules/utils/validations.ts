400893ccca60019c2169629e8ffaf8bbce604d23
reeli <reeli.null@gmail.com>
2018-09-04 22:14:33 +0800

    [Rui] exclude webpack.config.ts and other useless modules

--- src-modules/utils/validations.ts
+++ src-modules/utils/validations.ts
@@ -1,5 +1,5 @@
 import { size } from "lodash";
-import { TFieldValue } from "../rx-form/interfaces";
+import { TFieldValue } from "src";
 
 const createValidate = (isValid: (v: any) => boolean, defaultErrMsg: string) => {
   return (msg: string = defaultErrMsg) => {
