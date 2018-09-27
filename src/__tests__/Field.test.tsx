8807ba5a6c363dd0328c9f1615f82d9a66f4b629
reeli <reeli.null@gmail.com>
2018-09-27 21:51:07 +0800

    add more tests for Field component

--- src/__tests__/Field.test.tsx
+++ src/__tests__/Field.test.tsx
@@ -1,6 +1,7 @@
 import { mount } from "enzyme";
 import * as React from "react";
 import { Field } from "../Field";
+import { RxForm } from "../RxForm";
 import { pickInputPropsFromFieldProps } from "../utils";
 
 describe("<Field/>", () => {
@@ -15,6 +16,46 @@ describe("<Field/>", () => {
     expect(instance.state).toEqual({
       fieldState: {
         value: undefined,
+        meta: {
+          error: undefined,
+          dirty: false,
+        },
+      },
+    });
+  });
+
+  it("should set field value by initial form values in priority", () => {
+    const wrapper = mount(
+      <RxForm initialValues={{ firstName: "Jay" }}>
+        {() => (
+          <Field name={"firstName"} defaultValue={"Tony"} ref={"field"}>
+            {(fieldState) => <input type="text" {...pickInputPropsFromFieldProps(fieldState)} />}
+          </Field>
+        )}
+      </RxForm>,
+    );
+    const instance = wrapper.ref("field") as any;
+    expect(instance.state).toEqual({
+      fieldState: {
+        value: "Jay",
+        meta: {
+          error: undefined,
+          dirty: false,
+        },
+      },
+    });
+  });
+
+  it("should set field value by defaultValue prop if not have initialValues", () => {
+    const wrapper = mount(
+      <Field name={"firstName"} defaultValue={"Tony"}>
+        {(fieldState) => <input type="text" {...pickInputPropsFromFieldProps(fieldState)} />}
+      </Field>,
+    );
+    const instance = wrapper.children().instance() as any;
+    expect(instance.state).toEqual({
+      fieldState: {
+        value: "Tony",
         meta: {
           error: undefined,
           dirty: false,
