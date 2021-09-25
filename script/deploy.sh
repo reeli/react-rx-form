79a7a8c9d9dfc0251ca5861cc0ffe9e52b5c6770
reeli <reeli.null@gmail.com>
2021-09-25 15:13:13 +0800

    setup github pages

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,5 +4,5 @@ npm run release
 cp public/index.html public/200.html
 cp -R docs public
 
-npm install --global surge
-surge --project ./public --domain react-rx-form.surge.sh
+#npm install --global surge
+#surge --project ./public --domain react-rx-form.surge.sh
